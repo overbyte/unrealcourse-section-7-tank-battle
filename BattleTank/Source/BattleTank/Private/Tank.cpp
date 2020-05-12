883760b9a49c568cb399169591c26a67a6653a00
Allandt Bik-Elliott <allandt@gmail.com>
2020-05-12 15:02:48 +0100

    remove tick from tank

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -2,13 +2,14 @@
 
 
 #include "Tank.h"
+#include "TankAimingComponent.h"
 
 
 // Sets default values
 ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
     // NOTE: We are setting this in the constructor so don't need to protect pointer
     AimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
@@ -18,13 +19,6 @@ ATank::ATank()
 void ATank::BeginPlay()
 {
 	Super::BeginPlay();
-
-}
-
-// Called every frame
-void ATank::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
 
 }
 
