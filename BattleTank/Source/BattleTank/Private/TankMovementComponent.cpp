d5a296f69353072d701dc996f5dc5b2ab8128ad0
Allandt Bik-Elliott <allandt@gmail.com>
2020-05-13 17:23:45 +0100

    add stub TankMovementComponent

---
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -0,0 +1,8 @@
+// Copyright 2020 Allandt Bik-Elliott
+
+
+#include "TankMovementComponent.h"
+
+
+
+
---
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -0,0 +1,20 @@
+// Copyright 2020 Allandt Bik-Elliott
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
