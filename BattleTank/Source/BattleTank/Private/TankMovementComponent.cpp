31087dbfef7f690f9e95557ca5c64e1855cdab19
Allandt Bik-Elliott <allandt@gmail.com>
2020-05-15 12:18:10 +0100

    use dot product of movevelocity and current forward direction to move the AI tanks forward and backwards to the player tank.

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -27,9 +27,17 @@ void UTankMovementComponent::SetTurnRightIntent(float Throw)
     RightTrack->SetThrottle(-Throw);
 }
 
-
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
-    auto TankName = GetOwner()->GetName();
-    UE_LOG(LogTemp, Warning, TEXT("%s moving at %s"), *TankName, *MoveVelocity.ToString());
+    FVector ForwardDirection = GetOwner()->GetActorForwardVector().GetSafeNormal();
+
+    // convert velocity into a unit vector (total of 1)
+    FVector ForwardIntent = MoveVelocity.GetSafeNormal();
+
+    // get the dot product (https://en.wikipedia.org/wiki/Dot_product)
+    float DotProduct = FVector::DotProduct(ForwardDirection, ForwardIntent);
+    SetForwardIntent(DotProduct);
+
+    //auto TankName = GetOwner()->GetName();
+    //UE_LOG(LogTemp, Warning, TEXT("%s moving at %s"), *TankName, *MoveVelocity.ToString());
 }
